ffabbac2cfc23e139ec44010285237a34203a99b
Amrir Toufali <Amrir@HandsomeMedia.com>
2017-06-18 01:46:30 -0700

    beta (minus reset btn)

--- client/js/App.js
+++ client/js/App.js
@@ -6,43 +6,84 @@ import appStyles from '../sass/app.scss';
 
 const Autocomplete = {
 	init(api) {
+		const field = document.createElement('fieldset');
 		const ul = document.createElement('ul');
 		const txt = document.createElement('input');
 		txt.type = 'text';
 		txt.autofocus = true;
 		txt.placeholder = 'Search states';
 		ul.index = -1;
-		ul.addEventListener('click', handleUi);
-		this.form = document.createElement('form');
-		this.form.addEventListener('keyup', handleUi);
-		this.form.appendChild(txt);
-		this.form.appendChild(ul);
+		ul.hidden = true;
+		field.addEventListener('keyup', handleKeyboard);
+		field.addEventListener('click', handlePointer);
+		field.appendChild(txt);
+		field.appendChild(ul);
 
-		function handleUi(e) {
+		function handleKeyboard(e) {
 			switch (e.keyCode) {
+				case 9: // Tab
 				case 40: // Arrow Down
-					updateFocus(1);
+					focusList(1);
 					break;
 				case 38: // Arrow Up
-					updateFocus(-1);
+					focusList(-1);
 					break;
-				case 8: // Backspace
-					txt.bksp = true;
-					return;
-					break;
+				case 27: // Escape
+					txt.value = txt.startVal;
 				case 13: // Enter
-				case undefined: // Pointer device
-					hideSel(true);
+					hideList(true);
 					break;
 				default:
+					txt.focus();
 					updateList();
-					return;
 			}
-			e.preventDefault();
 		}
 
-		function updateFocus(num) {
-			ul.index = Math.min(Math.max(ul.index + num, -1), ul.children.length - 1);
+		function handlePointer(e) {
+			const target = e.target;
+			if (target.nodeName.toLowerCase() === 'li') {
+				txt.value = target.textContent;
+				hideList(true);
+			}
+		}
+
+		function updateList() {
+			if (txt.value === txt.startVal) {
+				return;
+			}
+			ul.index = -1;
+			txt.startVal = txt.value;
+			if (txt.value.length < 2) {
+				hideList(true);
+				return;
+			}
+
+			xhrReq('GET', `${api}?term=${txt.value}`)
+				.then((obj) => {
+					if (obj.count) {
+						hideList(false);
+					} else {
+						hideList(true);
+						return;
+					}
+					obj.data.forEach((item, i) => {
+						const li = ul.children[i] || document.createElement('li');
+						li.textContent = item.name;
+						if (!ul.contains(li)) {
+							li.setAttribute('tabindex', 0);
+							ul.appendChild(li);
+						}
+					});
+					while (ul.childElementCount > obj.count) {
+						ul.lastChild.remove();
+					}
+				}, (err) => console.log(err));
+		};
+
+		function focusList(num) {
+			if (ul.hidden) return;
+
+			ul.index = Math.min(Math.max(ul.index + num, -1), ul.childElementCount - 1);
 			if (ul.index === -1) {
 				txt.value = txt.startVal;
 				txt.focus();
@@ -52,42 +93,20 @@ const Autocomplete = {
 			}
 		}
 
-		function updateList() {
-			txt.startVal = txt.value;
-			while (ul.lastChild) {
-				ul.lastChild.remove();
-			}
-			if (txt.value.length < 2) return;
-			xhrReq('GET', `${api}?term=${txt.value}`)
-				.then((obj) => {
-					obj.data.forEach((item, i) => {
-						const li = document.createElement('li');
-						li.textContent = item.name;
-						li.setAttribute('tabindex', i + 1);
-						ul.appendChild(li);
-					});
-					ul.size = Math.min(5, obj.count);
-
-					if (ul.size === 1 && !txt.bksp) {
-						txt.value = ul.firstChild.textContent;
-						hideSel(true);
-					} else {
-						delete txt.bksp;
-						ul.selectedIndex = -1;
-						hideSel(false);
-					}
-				}, (err) => console.log(err));
-		};
-
-		function hideSel(hide) {
-			if (hide) {
-				ul.setAttribute('hidden', true);
+		function hideList(bool) {
+			if (bool) {
+				ul.hidden = true;
+				ul.index = -1;
+				txt.focus();
 			} else {
 				ul.removeAttribute('hidden');
 			}
 		}
 
-		return this;
+		return field;
+	},
+	clear() {
+		console.log(this);
 	},
 };
 
@@ -110,5 +129,7 @@ function xhrReq(method, url) {
 	}));
 }
 
-const stateSel = Object.create(Autocomplete).init('http://localhost:3000/api/states');
-document.getElementById('app').appendChild(stateSel.form);
+const stateField = Object.create(Autocomplete);
+document.getElementById('app').appendChild(stateField.init('http://localhost:3000/api/states'));
+stateField.clear();
+console.log(Autocomplete.isPrototypeOf(stateField));
